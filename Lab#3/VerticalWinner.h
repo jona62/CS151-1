3694a9306b48a27ffe1fc95f87b457cdebfb9d14
Jonathan James Mshelia <jonathanjamesm66@gmail.com>
2017-10-30 20:07:23 -0400

    Update VerticalWinner.h

--- Lab#3/VerticalWinner.h
+++ Lab#3/VerticalWinner.h
@@ -1,14 +1,23 @@
-//Team: <Team Name>
-//Author: <Names>
-//Creation: <Date>
+//Team: JO-EL
+//Author: Jonathan James
+//Creation: 10-30-17
 #ifndef VERTICALWINNER_H
 #define VERTICALWINNER_H
 
 #include "Archive.h"
 
-bool verticalWinner()
+bool verticalWinner( )
 {
+	if (( grid[3]==grid[0] && grid[3]== grid[6] && grid[0] == grid[6]) && (grid[0] != '-') && ( grid[3] != '-') && ( grid[6] != '-')){
+		return true;
+	}
+	else if (( grid[4] == grid[1] && grid[4] == grid[7] && grid[1] == grid[7]) && ( grid[1] != '-') && ( grid[4] != '-') && ( grid[7] != '-')){
+		return true;
+	}
+	else if (( grid[5] == grid[2] && grid[5] == grid[8] && grid[2] == grid[8]) && ( grid[2] != '-') && ( grid[5] != '-') && ( grid[8] != '-')){
+			return true;
+		}
 	return false;
 }
 
-#endif 
+#endif
