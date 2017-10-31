8daf0df0df1ed18e3b2af2f58c7bfb5981abb784
Jonathan James Mshelia <jonathanjamesm66@gmail.com>
2017-10-30 20:09:25 -0400

    Update DiagonalWinner.h

--- Lab#3/DiagonalWinner.h
+++ Lab#3/DiagonalWinner.h
@@ -1,6 +1,6 @@
-//Team: <Team Name>
-//Author: <Names>
-//Creation: <Date>
+//Team: JO-EL
+//Author: Jonathan James
+//Creation: 10-30-17
 #ifndef DIAGONALWINNER_H
 #define DIAGONALWINNER_H
 
@@ -8,7 +8,14 @@
 
 bool diagonalWinner()
 {
+	if (( grid[4] == grid[2] && grid[4] == grid[6] && grid[6] == grid[2]) && (grid[2] != '-') && (grid[4] != '-') && (grid[6] != '-')){
+		return true;
+	}
+	else if (( grid[4] == grid[0] && grid[4] == grid[8] && grid[0] == grid[8]) && (grid[0] != '-') && (grid[4] != '-') && (grid[8] != '-')){
+		return true;
+	}
+
 	return false;
 }
 
-#endif 
+#endif
