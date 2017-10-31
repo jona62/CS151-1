dead2070b43c14d4bfb36963f10a07ef84f21907
Jonathan James Mshelia <jonathanjamesm66@gmail.com>
2017-10-30 20:06:43 -0400

    Update Board.h

--- Lab#3/Board.h
+++ Lab#3/Board.h
@@ -1,6 +1,6 @@
-//Team: <Team Name>
-//Author: <Names>
-//Creation: <Date>
+//Team: JO-EL
+//Author: Jonathan James
+//Creation: 10-30-17
 #ifndef BOARD_H
 #define BOARD_H
 
@@ -8,7 +8,26 @@
 
 std::string board()
 {
-	return "";
+	std::string result = "\n";
+	result += grid[0];
+	result += " ";
+	result += grid[1];
+	result += " ";
+	result += grid[2];
+	result += "\n";
+	result += grid[3];
+	result += " ";
+	result += grid[4];
+	result += " ";
+	result += grid[5];
+	result += "\n";
+	result += grid[6];
+	result += " ";
+	result += grid[7];
+	result += " ";
+	result += grid[8];
+	result += "\n";
+	return result;
 }
 
-#endif 
+#endif
