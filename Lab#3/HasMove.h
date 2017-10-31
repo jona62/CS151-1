1823de70ccd16a56a488cec0ed60bcd1627e68e3
Jonathan James Mshelia <jonathanjamesm66@gmail.com>
2017-10-30 20:08:54 -0400

    Update HasMove.h

--- Lab#3/HasMove.h
+++ Lab#3/HasMove.h
@@ -1,6 +1,6 @@
-//Team: <Team Name>
-//Author: <Names>
-//Creation: <Date>
+//Team: JO-EL
+//Author: Jonathan James
+//Creation: 10-30-17
 #ifndef HASMOVE_H
 #define HASMOVE_H
 
@@ -8,7 +8,9 @@
 
 bool hasMove()
 {
+	if (grid[0] == '-' || grid[1] == '-' || grid[2] == '-' || grid[3] == '-' || grid[4] == '-' || grid[5] == '-' || grid[6] == '-' || grid[7] == '=' || grid[8] == '-'){
+		return true;
+	}
 	return false;
 }
-
-#endif 
+#endif
